baeaab79cae25cfc519c9e2d00668a568a543add
M. Darnell <msdarnell@gmail.com>
2020-09-20 19:33:09 -0400

    Add author to blog.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -9,22 +9,22 @@ import SEO from '../components/seo';
 const Pagination = (props) => (
   <div className="pagination -post">
     <ul>
-        {(props.previous && props.previous.frontmatter.template === 'blog-post') && (
-          <li>
-              <Link to={props.previous.frontmatter.slug} rel="prev">
-                <p><span className="icon -left"><RiArrowLeftLine/></span> Previous</p>
-                <span className="page-title">{props.previous.frontmatter.title}</span>
-              </Link>
-          </li>
-        )}
-        {(props.next && props.next.frontmatter.template === 'blog-post') && (
-          <li>
-            <Link to={props.next.frontmatter.slug} rel="next">
-              <p>Next <span className="icon -right"><RiArrowRightLine/></span></p>
-              <span className="page-title">{props.next.frontmatter.title}</span>
-            </Link>
-          </li>
-        )}
+      {(props.previous && props.previous.frontmatter.template === 'blog-post') && (
+        <li>
+          <Link to={props.previous.frontmatter.slug} rel="prev">
+            <p><span className="icon -left"><RiArrowLeftLine /></span> Previous</p>
+            <span className="page-title">{props.previous.frontmatter.title}</span>
+          </Link>
+        </li>
+      )}
+      {(props.next && props.next.frontmatter.template === 'blog-post') && (
+        <li>
+          <Link to={props.next.frontmatter.slug} rel="next">
+            <p>Next <span className="icon -right"><RiArrowRightLine /></span></p>
+            <span className="page-title">{props.next.frontmatter.title}</span>
+          </Link>
+        </li>
+      )}
     </ul>
   </div>
 )
@@ -55,8 +55,8 @@ const Post = ({ data, pageContext }) => {
             <time>{frontmatter.date}</time>
           </section>
           {Image ? (
-            <Img 
-              fluid={Image} 
+            <Img
+              fluid={Image}
               objectFit="cover"
               objectPosition="50% 50%"
               alt={frontmatter.title + ' - Featured image'}
@@ -64,7 +64,7 @@ const Post = ({ data, pageContext }) => {
             />
           ) : ""}
         </header>
-        
+        <p style="text-align:text-center;">{frontmatter.author}</p>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
