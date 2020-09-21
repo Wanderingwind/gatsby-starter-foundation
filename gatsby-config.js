d2733e0de1b8f3f21406ae570b943b6b2a1876dd
M. Darnell <msdarnell@gmail.com>
2020-09-21 06:41:49 -0400

    tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,12 +74,6 @@ module.exports = {
       options: {
         trackingId: settings.ga,
       },
-      resolve: `gatsby-plugin-sass`,
-      options: {
-        postCssPlugins: [
-          require("tailwindcss"),
-        ],
-      },
     },
     `gatsby-plugin-advanced-sitemap`,
     {
