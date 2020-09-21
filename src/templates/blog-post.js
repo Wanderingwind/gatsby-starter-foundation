da593825107a357533edfbc0f187319845ea3b77
M. Darnell <msdarnell@gmail.com>
2020-09-20 20:13:27 -0400

    Try again

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -65,7 +65,7 @@ const Post = ({ data, pageContext }) => {
           ) : ""}
 
         </header>
-        <p className="text-center">Author</p>
+        <p className="text-center">Author {frontmatter.author} {data.frontmatter.author}</p>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
