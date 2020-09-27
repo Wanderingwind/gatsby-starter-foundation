cb40c7b561e5c728a615a127d3ab415b531a0e77
M. Darnell <msdarnell@gmail.com>
2020-09-27 16:00:36 -0400

    Add all dependencies.

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -44,56 +44,40 @@ const HomePage = ({ data }) => {
       <div className="flex flex-col justify-between max-w-xl px-4 mx-auto lg:pt-16 lg:flex-row md:px-8 lg:max-w-screen-xl">
         <div className="pt-16 mb-16 lg:mb-0 lg:pt-32 lg:max-w-lg lg:pr-5">
           <div className="max-w-xl mb-6">
-            <div>
-              <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-                Brand new
-            </p>
-            </div>
             <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-              The quick, brown fox
+              Writer. Teacher. Mother.
             <br className="hidden md:block" />
-            jumps over{' '}
+            A bit of a {' '}
               <span className="inline-block text-deep-purple-accent-400">
-                a lazy dog
+                book-pusher.
             </span>
             </h2>
             <p className="text-base text-gray-700 md:text-lg">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae. explicabo.
+              I like to write and talk about education, parenting, film, theatre, young adult literature, sci-fi/geek culture, and food. Any of these combined? Even better.
           </p>
           </div>
           <div className="flex items-center">
-            <a
-              href="/"
-              className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-            >
-              Get started
-          </a>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-            >
-              Learn more
-          </a>
+            <Link to={frontmatter.cta.ctaLink} className="inline-flex mr-0 md:mr-4 items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none">{frontmatter.cta.ctaText}<span class="icon -right"><RiArrowRightSLine /></span></Link>
+            <Link to="/about" className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">About Me</Link>
           </div>
         </div>
-        <div>
-          <img
-            src="https://kitwind.io/assets/kometa/two-thirds-phone.png"
-            className="object-cover object-top w-full h-64 mx-auto lg:h-auto xl:mr-24 md:max-w-sm"
-            alt=""
+
+        {Image ? (
+          <Img
+            fluid={Image}
+            alt={frontmatter.title + ' - Featured image'}
+            className="object-cover object-top w-full h-64 mx-auto lg:h-auto xl:mr-24 md:max-w-lg featured-image"
           />
-        </div>
+        ) : ""}
+
       </div>
       <div className="home-banner grids col-1 sm-2">
         <div>
-          <h1 class="title">{frontmatter.title}</h1>
-          <p class="tagline">{frontmatter.tagline}</p>
+          <h1 className="title">{frontmatter.title}</h1>
+          <p className="tagline">{frontmatter.tagline}</p>
           <div className="description" dangerouslySetInnerHTML={{ __html: html }} />
           <Link to={frontmatter.cta.ctaLink} className="button">{frontmatter.cta.ctaText}<span class="icon -right"><RiArrowRightSLine /></span></Link>
-          <Link to={frontmatter.cta.ctaLink} className="button inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none">{frontmatter.cta.ctaText}<span class="icon -right"><RiArrowRightSLine /></span></Link>
+          <Link to={frontmatter.cta.ctaLink} className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none">{frontmatter.cta.ctaText}<span class="icon -right"><RiArrowRightSLine /></span></Link>
         </div>
         <div>
           {Image ? (
