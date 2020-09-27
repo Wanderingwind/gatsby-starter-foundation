0496783f49ed182d953b97cb48e0a5c65a1886c7
M. Darnell <msdarnell@gmail.com>
2020-09-27 12:16:35 -0400

    Four

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
+import "tailwindcss/base.css"
+import "tailwindcss/components.css"
+import "tailwindcss/utilities.css"
+
 export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
     `This application has been updated. ` +
