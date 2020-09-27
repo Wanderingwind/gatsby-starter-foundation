b0af033e78e13e5746031ea050e0fb879d4b78ad
M. Darnell <msdarnell@gmail.com>
2020-09-27 12:00:37 -0400

    Add Tailwind

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -47,6 +47,12 @@ const HomePage = ({ data }) => {
           <p class="tagline">{frontmatter.tagline}</p>
           <div className="description" dangerouslySetInnerHTML={{ __html: html }} />
           <Link to={frontmatter.cta.ctaLink} className="button">{frontmatter.cta.ctaText}<span class="icon -right"><RiArrowRightSLine /></span></Link>
+          <a
+            href="/"
+            className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+          >
+            Learn more
+            </a>
         </div>
         <div>
           {Image ? (
