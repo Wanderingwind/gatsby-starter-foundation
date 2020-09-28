dfa09aa1bec135cf1f108ec4354a7e3f82f05c72
M. Darnell <msdarnell@gmail.com>
2020-09-28 11:17:12 -0400

    Icon change

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,16 +80,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Foundation`,
-        short_name: `Foundation`,
+        name: `Madeleine Deliee`,
+        short_name: `MDeliee`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `static/assets/stackrole.png`,
+        icon: `static/assets/Favicon.png`,
       },
     },
-    'gatsby-plugin-offline',
     `gatsby-plugin-postcss`,
   ],
 }
