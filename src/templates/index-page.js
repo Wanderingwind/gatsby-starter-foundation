adcc077c6dc6493072be3d7816c9d74546fdac09
M. Darnell <msdarnell@gmail.com>
2020-10-02 08:40:55 -0400

    Fixed?

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -46,28 +46,8 @@ const HomePage = ({ data }) => {
           <div className="max-w-xl mb-6">
             <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
               <span className="relative inline-block">
-                <svg
-                  viewBox="0 0 52 24"
-                  fill="currentColor"
-                  className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-                >
-                  <defs>
-                    <pattern
-                      id="27df4f81-c854-45de-942a-fe90f7a300f9"
-                      x="0"
-                      y="0"
-                      width=".135"
-                      height=".30"
-                    >
-                      <circle cx="1" cy="1" r=".7" />
-                    </pattern>
-                  </defs>
-                  <rect
-                    fill="url(#27df4f81-c854-45de-942a-fe90f7a300f9)"
-                    width="52"
-                    height="24"
-                  />
-                </svg>
+
+
                 <span className="relative">Writer.</span>
               </span>{' '}
               Teacher. Mother.
@@ -127,7 +107,8 @@ const HomePage = ({ data }) => {
                   height="24"
                 />
               </svg>
-              <span className="relative">Articles</span>
+
+              <span className="relative">Articles,</span>
             </span>{' '}
           blogs, reviews, random thoughts and such
         </h2>
