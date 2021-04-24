1e9becec88065de166ba72db9b4529b96e5c6b7e
M. Darnell <msdarnell@gmail.com>
2021-04-24 07:22:30 -0400

    s

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>Thanks for reading. </p>
+      <p>Thanks for reading! </p>
     </div>
   </footer>
 )
