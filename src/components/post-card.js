789c130d0298aaf5e313f08a016b8faaa905b92f
M. Darnell <msdarnell@gmail.com>
2021-04-24 07:04:51 -0400

    tracking tweak

--- src/components/post-card.js
+++ src/components/post-card.js
@@ -22,7 +22,7 @@ const PostCard = ({ data }) => (
       <p className="metamb-3 text-xs font-semibold tracking-wide uppercase">
         <time>{data.frontmatter.date}</time>
       </p>
-      <h2 className="title inline-block mb-3 text-2xl font-bold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-700">
+      <h2 className="title inline-block mb-3 text-2xl font-bold leading-8 transition-colors duration-200 hover:text-deep-purple-accent-700">
         <Link to={data.frontmatter.slug}>{data.frontmatter.title}</Link>
       </h2>
       <p className="meta mb-2 text-gray-700">
